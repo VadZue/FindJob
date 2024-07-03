b9fa8006d8c98c4abd498e3c36e2a532f4aaada0
pstlnce <galvanistic.reanimation@yandex.com>
2024-07-04 00:33:46 +0300

    vacancies pulling

---
+++ FindJob/Database.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FindJob;
+
+internal class Database : DbContext
+{
+    public Database(DbContextOptions<Database> options) : base(options)
+    { }
+
+    public DbSet<Vacansy> Vacansies => Set<Vacansy>();
+}
